22b89aea747414e42bd73fbb40a7890fed880dbd
kmu-chaseoyeon <118155130+wantpro@users.noreply.github.com>
2025-07-23 15:13:33 +0900

    Update main.dart
    회원가입 페이지 연결 위해 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
+import 'package:login_fin/login_screen.dart';  // 로그인 페이지 임포트
+import 'package:firebase_core/firebase_core.dart';
+import 'package:login_fin/SignUpPage.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,58 +20,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '지갑을 부탁해',
-      debugShowCheckedModeBanner: false,
+      initialRoute: LoginPageWidget.routePath,  // 로그인 페이지를 초기화면으로 지정
+      routes: {
+        LoginPageWidget.routePath: (context) => const LoginPageWidget(),
+        '/signup': (context) => const SignUpPage(), // 가입 페이지
+        // '/home': (context) => const HomePage(), // 로그인 후 메인 페이지. 이후에 추가 바람.
+      },
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2797FF),
       ),
-      home: const LoginPageWidget(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
