b89fc94e05c138545af196881ab91ab30cda22c9
Zzong99 <moondain0128@naver.com>
2025-07-23 19:20:36 +0900

    Update manual_entry_screen.dart
    카테고리 추가

--- screens/manual_entry_screen.dart
+++ screens/manual_entry_screen.dart
@@ -2,21 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class ManualEntryScreen extends StatefulWidget {
-  final Function(Transaction) onAdd;
-
-  ManualEntryScreen({required this.onAdd});
-
-  @override
-  _ManualEntryScreenState createState() => _ManualEntryScreenState();
-}
-
 class _ManualEntryScreenState extends State<ManualEntryScreen> {
   final _formKey = GlobalKey<FormState>();
   String _type = '지출';
-  String _category = '';
+  String _category = '식비'; // 초기값 변경
   int _amount = 0;
   DateTime _selectedDate = DateTime.now();
+
+  final List<String> _categoryOptions = [
+    '식비', '취미-여가', '의료', '교통', '생활', '쇼핑',
+  ];
 
   void _submit() {
     if (_formKey.currentState!.validate()) {
@@ -42,18 +37,20 @@ class _ManualEntryScreenState extends State<ManualEntryScreen> {
           child: Column(children: [
             DropdownButtonFormField<String>(
               value: _type,
-              items: ['수입', '지출']
-                  .map((label) => DropdownMenuItem(
-                        child: Text(label),
-                        value: label,
-                      ))
-                  .toList(),
+              items: ['수입', '지출'].map((label) => DropdownMenuItem(
+                child: Text(label),
+                value: label,
+              )).toList(),
               onChanged: (value) => setState(() => _type = value!),
             ),
-            TextFormField(
-              decoration: InputDecoration(labelText: "항목"),
-              onSaved: (value) => _category = value!,
-              validator: (value) => value!.isEmpty ? '항목 입력' : null,
+            DropdownButtonFormField<String>(
+              value: _category,
+              items: _categoryOptions.map((label) => DropdownMenuItem(
+                child: Text(label),
+                value: label,
+              )).toList(),
+              onChanged: (value) => setState(() => _category = value!),
+              decoration: InputDecoration(labelText: "카테고리"),
             ),
             TextFormField(
               decoration: InputDecoration(labelText: "금액"),
