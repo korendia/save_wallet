cbfad87c5df52b5feaa1e388aefcdfaa06fedf38
Zzong99 <moondain0128@naver.com>
2025-07-23 19:14:00 +0900

    Create transaction.dart

---
+++ models/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String type; // '수입' 또는 '지출'
+  final String category;
+  final int amount;
+  final DateTime date;
+
+  Transaction({
+    required this.type,
+    required this.category,
+    required this.amount,
+    required this.date,
+  });
+}
