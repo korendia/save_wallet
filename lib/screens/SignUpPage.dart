a9b4a5d7245dbac899196572b0b29c70d9438d00
kmu-chaseoyeon <118155130+wantpro@users.noreply.github.com>
2025-07-23 20:43:22 +0900

    코드에 설명 주석 달음
    어디 남겨야할지 and 만든 함수 간단설명.

--- lib/screens/SignUpPage.dart
+++ lib/screens/SignUpPage.dart
@@ -31,7 +31,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   child: SizedBox(
                     width: 300,
                     child: CupertinoTextField(
-                      controller: userIdController,
+                      controller: userIdController, //이메일 입력한 문자열이 저장되는 부분. firebase에 저장할 때 쓰이므로 이 부분 유지할것.
                       placeholder: '아이디를 입력해주세요',
                       textAlign: TextAlign.center,
                     ),
@@ -43,7 +43,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   child: SizedBox(
                     width: 300,
                     child: CupertinoTextField(
-                      controller: passwordController,
+                      controller: passwordController, //비밀번호 입력한 문자열이 저장되는 부분. firebase에 저장할 때 쓰이므로 이 부분 유지할것.
                       placeholder: '비밀번호를 입력해주세요',
                       textAlign: TextAlign.center,
                       obscureText: true,
@@ -56,7 +56,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   child: SizedBox(
                     width: 300,
                     child: CupertinoTextField(
-                      controller: passwordVerifyingController,
+                      controller: passwordVerifyingController,//비밀번호 재확인용으로 입력한 문자열이 저장되는 부분. 처음 입력한 비밀번호와 확인 비밀번호가 같은지 판별하기 위해 쓰임.
                       placeholder: '비밀번호를 다시 입력해주세요',
                       textAlign: TextAlign.center,
                       obscureText: true,
@@ -84,9 +84,9 @@ class _SignUpPageState extends State<SignUpPage> {
                         width: 195,
                         child: ElevatedButton(
                           onPressed: () async{
-                            var check = confirmIdCheck(context, passwordController.text, passwordVerifyingController.text);
+                            var check = confirmIdCheck(context, passwordController.text, passwordVerifyingController.text); //입력한 비밀번호와 확인 비밀번호가 같은지 확인하고 같으면 1 틀리면 0 리턴.
                             if (check == 1){
-                              await signup(context, userIdController.text, passwordController.text);
+                              await signup(context, userIdController.text, passwordController.text); //두 비밀번호가 같으면 파이어베이스에 정보 저장하는 함수. 자세한 설명은 login_service 참고.
                             }
                           },
                           child: Text('계정 생성'),
