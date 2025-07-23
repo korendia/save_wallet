da3deb53870222ffcef7b7207dcefde6b8acd70e
kmu-chaseoyeon <118155130+wantpro@users.noreply.github.com>
2025-07-23 15:14:23 +0900

    Add files via upload

---
+++ lib/screens/SignUpPage.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:login_fin/login_service.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  // 유저의 아이디와 비밀번호의 정보 저장
+  final TextEditingController userIdController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController passwordVerifyingController =
+  TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          // 아이디 입력 텍스트필드
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 300,
+                    child: CupertinoTextField(
+                      controller: userIdController,
+                      placeholder: '아이디를 입력해주세요',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                // 비밀번호 입력 텍스트필드
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 300,
+                    child: CupertinoTextField(
+                      controller: passwordController,
+                      placeholder: '비밀번호를 입력해주세요',
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                    ),
+                  ),
+                ),
+                // 비밀번호 재확인 텍스트필드
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 300,
+                    child: CupertinoTextField(
+                      controller: passwordVerifyingController,
+                      placeholder: '비밀번호를 다시 입력해주세요',
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                    ),
+                  ),
+                ),
+                // 로그인 페이지로 돌아가기
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 95,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('뒤로 가기'),
+                        ),
+                      ),
+                      Text('   '),
+                      // 계정 생성 버튼
+                      SizedBox(
+                        width: 195,
+                        child: ElevatedButton(
+                          onPressed: () async{
+                            var check = confirmIdCheck(context, passwordController.text, passwordVerifyingController.text);
+                            if (check == 1){
+                              await signup(context, userIdController.text, passwordController.text);
+                            }
+                          },
+                          child: Text('계정 생성'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
