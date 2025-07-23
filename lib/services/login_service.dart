9bc62e4573f1490f2e6e598a183c642999804a25
kmu-chaseoyeon <118155130+wantpro@users.noreply.github.com>
2025-07-23 20:47:50 +0900

    Update login_service.dart
    함수 동작 원리 설명

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -7,7 +7,8 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 
 Future<void> login(BuildContext context, String email, String password)async {
   try {
-    await _auth.signInWithEmailAndPassword(email: email, password: password);
+    await _auth.signInWithEmailAndPassword(email: email, password: password); //firebase에 입력받은 정보 보내서 그쪽 데이터와 비교해보는 작업.
+    //정상 실행되었으면 아래쪽 실행
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text('로그인 성공'),
@@ -17,18 +18,21 @@ Future<void> login(BuildContext context, String email, String password)async {
         //Navigator.of(context).push(
         //  MaterialPageRoute(builder: (context) => NewPage()),
         //);
-        //화면 전환 부분. 알아서 만들어주면 됨
+        //로그인이 되었으니 화면을 전환하는 부분. 알아서 만들어주면 됨
     );
   } catch (e) {
+    //오류가 났으면 이 부분 실행
     if (email.isEmpty || password.isEmpty){
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
+            //패스워드, 비번 미입력이라면 이부분 실행
             content: Text('비밀번호 혹은 패스워드가 입력되지 않았습니다.'),
             duration: Duration(seconds: 2),
           )
       );
     }
     else {
+      //그 외 다른 에러라면 여기 실행(영어임)
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('오류. 사유 : $e'),
@@ -39,9 +43,11 @@ Future<void> login(BuildContext context, String email, String password)async {
   }
 }
 
-Future<void> signup(BuildContext context, String email, String password)async {
+Future<void> signup(BuildContext context, String email, String password)async { //회원가입
   try {
+    //입력받은 정보 firebase 전송
     await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    //정상작동 이 부분 실행
     ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('회원가입 성공'),
@@ -49,6 +55,7 @@ Future<void> signup(BuildContext context, String email, String password)async {
         )
     );
   } catch (e) {
+    //오류나면 이 부분 실행
     ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text('회원가입 실패 : $e'),
@@ -58,6 +65,7 @@ Future<void> signup(BuildContext context, String email, String password)async {
   }
 }
 
+//얘는 그냥 비밀번호 재입력한거랑 처음 입력한 게 같은지 다른지 판별하는 용도
 int confirmIdCheck(BuildContext context, String password, String passwordVerifying) {
   if (password != passwordVerifying){
     ScaffoldMessenger.of(context).showSnackBar(
@@ -74,7 +82,7 @@ int confirmIdCheck(BuildContext context, String password, String passwordVerifyi
   }
 
 
-  //항목마다 합산 금액 나타내는 거.
+  //입력받은 항목마다 합산 금액 나타내는 거.
 /*
   Future<int> analizing_money(String item) async{
     final snapshot = await FirebaseFirestore.instance
