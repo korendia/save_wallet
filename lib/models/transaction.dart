8baeaf126120606a58653ed3f147028c7568dd2c
kmu-chaseoyeon <118155130+wantpro@users.noreply.github.com>
2025-08-05 09:54:36 +0900

    Update transaction.dart
    파이어베이스에 동일한 이름의 클래스가 있어 뒤에 언더바 붙여서 구분함

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,10 +1,10 @@
-class Transaction {
+class Transaction_ {
   final String type; // '수입' 또는 '지출'
   final String category;
   final int amount;
   final DateTime date;
 
-  Transaction({
+  Transaction_({
     required this.type,
     required this.category,
     required this.amount,
