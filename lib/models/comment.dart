0bd0e470c7aa19282c46b434f68f0c067662a9a6
korendia <ih.bae25@gmail.com>
2025-08-12 20:52:46 +0900

    Create comment.dart community_screen.dart에 사용되는 model추가

---
+++ lib/models/comment.dart
@@ -0,0 +1,13 @@
+class Comment {
+  final String id;
+  final String author;
+  final String content;
+  final DateTime createdAt;
+
+  Comment({
+    required this.id,
+    required this.author,
+    required this.content,
+    required this.createdAt,
+  });
+}
